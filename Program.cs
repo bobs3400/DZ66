bc7e0f1d42d98ee59c1813c746b7034718f9d0e7
bobs3400 <124318533+bobs3400@users.noreply.github.com>
2023-03-11 16:49:00 +1000

    66

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+Clear();
+Write("Введите число M: ");
+int m = int.Parse(ReadLine()!);
+Write("Введите число N: ");
+int n = int.Parse(ReadLine()!);
+
+WriteLine();
+WriteLine($"M = {m}; N = {n} -> {SumNumbers(m, n)}");
+
+int SumNumbers(int m, int n)
+{   
+    if(n == m)
+        return m;
+    return (n + SumNumbers(m, n - 1));
+}
